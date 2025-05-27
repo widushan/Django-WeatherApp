7cf08e982bb4e56f5c652b76c916ca4b17b7ebe6
widushan <114930601+widushan@users.noreply.github.com>
2025-05-28 00:31:19 +0530

    main commit
    get weather details and display them in a basic web page

--- weather/weatherapp/views.py
+++ weather/weatherapp/views.py
@@ -1,3 +1,33 @@
+from datetime import datetime
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def home(request):
+    city = ''
+    temp = ''
+    description = ''
+    icon = ''
+    day = ''
+
+    if request.method == 'POST':
+        city = request.POST.get('city')
+        # Example API call (replace with your actual logic)
+        # Example: Using OpenWeatherMap API
+        API_KEY = 'ffc5d1d87dc9e9cdbcaa94559a9949b1'
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
+        response = requests.get(url)
+        data = response.json()
+
+        if data['cod'] == 200:
+            temp = data['main']['temp']
+            description = data['weather'][0]['description']
+            icon = data['weather'][0]['icon']
+            day = datetime.fromtimestamp(data['dt'])
+
+    return render(request, 'index.html', {
+        'city': city,
+        'temp': temp,
+        'description': description,
+        'icon': icon,
+        'day': day,
+    })
